215079a81af4828655740d3dc2c3c49b7d22e0ab
Cyril POIDEVIN <vilsafur@gmail.com>
2016-10-28 16:41:48 +0200

    Added checking given path.

--- test/main.js
+++ test/main.js
@@ -12,10 +12,20 @@
  
  describe('frl', function(){
    describe('with no argument', function() {
-     it('returns console log to inform path not found', function(done) {
+     it('returns console log to inform path not specified', function(done) {
        var result = frl('', function(result) {
          result.should.eql('The path was not specified');
          done();
+       });
+     });
+   });
+   describe('with a path', function() {
+     describe('invalid', function() {
+       it('return console log to inform path is invalid', function(done) {
+         var result = frl('Hello', function(result) {
+           result.should.eql('The given path do not exist');
+           done();
+         });
        });
      });
    });
